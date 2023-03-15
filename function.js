3d42a44f2e4648d15cd769fa040cda51280ee7b3
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-15 22:52:48 +0100

    testing

--- function.js
+++ function.js
@@ -6,4 +6,32 @@ wenn eingabe falsch war, dann Fehler zurück geben
 
 */
 
-function rechnen ()
+function rechnen(zahl1, zahl2, paraRechenZeichen) {
+    let z = paraRechenZeichen
+    let ergebniss = 0
+    if ((zahl1 >= 0 && zahl2 >= 0) && (z == "+" || z == "-" || z == "/" || z == "*")) {
+        if (z == "+") {
+            ergebniss = zahl1 + zahl2
+
+        } 
+        else if (z == "-") {
+            ergebniss = zahl1 - zahl2
+        }
+        else if (z == "/") {
+            ergebniss = zahl1 / zahl2
+        }
+        else if (z == "*") {
+            ergebniss = zahl1 * zahl2
+        }
+
+        return { wert : ergebniss,
+                boolean : true, }
+    } else {
+        return [false, "wiederhole die Eingabe"]
+    }
+}
+
+
+/* export default rechnen
+ */
+export {rechnen}
---
+++ index.js
@@ -0,0 +1,4 @@
+/* import rechnen from "./function.js";
+ */import {rechnen}  from "./function.js";
+
+console.log(rechnen(1,"z","*"))
